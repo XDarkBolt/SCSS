4fbff3967d2172da3b5b1c355239db63f185e6e6
XDarkBolt <kayahuseyin45@hotmail.com>
2022-11-02 12:47:05 +0300

    axios

---
+++ myscript.js
@@ -0,0 +1,15 @@
+axios.get("https://catfact.ninja/fact")
+    .then(function (response) {
+        document.getElementById("description").innerHTML = response.data.fact;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+axios.get("https://api.thecatapi.com/v1/images/search")
+    .then(function (response) {
+        document.getElementById("img").src = response.data[0].url;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
