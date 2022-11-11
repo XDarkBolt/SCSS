29f62f8e9e15adb908f7ec527f0af3804e8c8735
XDarkBolt <kayahuseyin45@hotmail.com>
2022-11-11 17:50:03 +0300

    glass

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 axios.get("https://catfact.ninja/fact")
     .then(function (response) {
         document.getElementById("description").innerHTML = response.data.fact;
+        document.getElementById("description2").innerHTML = response.data.fact;
     })
     .catch(function (error) {
         console.log(error);
@@ -9,6 +10,7 @@ axios.get("https://catfact.ninja/fact")
 axios.get("https://api.thecatapi.com/v1/images/search")
     .then(function (response) {
         document.getElementById("img").src = response.data[0].url;
+        document.getElementById("img2").src = response.data[0].url;
     })
     .catch(function (error) {
         console.log(error);
